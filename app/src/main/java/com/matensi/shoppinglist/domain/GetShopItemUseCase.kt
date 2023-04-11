b58baae6c445f4dcfaba7fe903a80c792c1ee060
Matensi <hanspridevitebsk@mail.ru>
2023-04-11 14:08:49 +0300

    AddDataLayer

---
+++ app/src/main/java/com/matensi/shoppinglist/domain/GetShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.matensi.shoppinglist.domain
+
+class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
+    fun getShopItem(shopItemId: Int): ShopItem {
+        return shopListRepository.getShopItem(shopItemId)
+    }
+}
