72868d40a3bb3afc43d85c52eeda544c13b76ae5
Matensi <hanspridevitebsk@mail.ru>
2023-04-11 13:22:36 +0300

    CleanArchCreate

---
+++ app/src/main/java/com/matensi/shoppinglist/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.matensi.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.matensi.shoppinglist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
